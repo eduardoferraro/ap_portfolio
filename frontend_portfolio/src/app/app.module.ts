872b0aec348a12f742ebb8840183a67515260031
Eduardo <eduard.ferraro@gmail.com>
2022-08-15 16:56:30 -0300

    secciones separadas en componentes

--- frontend_portfolio/src/app/app.module.ts
+++ frontend_portfolio/src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { AcercademiComponent } from './componentes/acercademi/acercademi.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { PiedepaginaComponent } from './componentes/piedepagina/piedepagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    MenuComponent,
+    AcercademiComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
+    PiedepaginaComponent
   ],
   imports: [
     BrowserModule
---
+++ frontend_portfolio/src/app/componentes/menu/menu.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var window: any;
+
+@Component({
+    selector: 'app-menu',
+    templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+    
+    //modal
+    loginModal: any;
+
+    constructor() { }
+
+    ngOnInit(): void {
+        //modal
+        this.loginModal = new window.bootstrap.Modal(
+            document.getElementById('login-modal')
+        );
+    }
+
+    //modal
+    abrirLoginModal() {
+        this.loginModal.show();
+    }
+    
+    
+}
